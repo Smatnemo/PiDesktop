9dd65876fd92f826dd8e98248153c726fb4f7853
Tertese Moses <smatnemo25@gmail.com>
2023-11-02 15:33:39 +0000

    Created legal module

---
+++ legal.py
@@ -0,0 +1,28 @@
+from lds.database import DataBase 
+from lds.utils import LOGGER
+from lds.view.window import PiWindow
+
+class PiApplication:
+    def __init__(self):
+        pass 
+
+    def _initialize(self):
+        pass 
+
+    def main_loop(self):
+        try:
+            pass
+        except Exception as e:
+            # Log error
+            LOGGER.error()
+            # Get crash message
+            LOGGER.error()
+        finally:
+            pass
+
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
