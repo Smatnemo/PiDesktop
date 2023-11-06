40d941e5d8b1b1964b9f33024ddc28785a0a9ea7
Tertese Moses <smatnemo25@gmail.com>
2023-11-06 22:18:04 +0000

    Created get_translated_text, get_supported_languages, edit

--- language.py
+++ language.py
@@ -190,3 +190,86 @@ DEFAULT = {
         'oops': "Oops, ocorreu um erro.",
     },
 }
+
+def init(filename, clear=False):
+    """Initialize the translation system.
+
+    :param filename: path to the translations file
+    :type filename: str
+    :param clear: restore default translations
+    :type clear: bool
+    """
+    PARSER.filename = osp.abspath(osp.expanduser(filename))
+
+    if not osp.isfile(PARSER.filename) or clear:
+        LOGGER.info("Generate the translation file in '%s'", PARSER.filename)
+        dirname = osp.dirname(PARSER.filename)
+        if not osp.isdir(dirname):
+            os.makedirs(dirname)
+
+        with io.open(PARSER.filename, 'w', encoding="utf-8") as fp:
+            for section, options in DEFAULT.items():
+                fp.write("[{}]\n".format(section))
+                for name, value in options.items():
+                    value = value.splitlines()
+                    fp.write("{} = {}\n".format(name, value[0]))
+                    if len(value) > 1:
+                        for part in value[1:]:
+                            fp.write("    {}\n".format(part))
+                fp.write("\n\n")
+
+    PARSER.read(PARSER.filename, encoding='utf-8')
+
+    # Update the current file with missing language(s) and key(s)
+    changed = False
+    for section, options in DEFAULT.items():
+        if not PARSER.has_section(section):
+            changed = True
+            LOGGER.debug("Add [%s] to available language list", section)
+            PARSER.add_section(section)
+
+        for option, value in options.items():
+            if not PARSER.has_option(section, option):
+                changed = True
+                LOGGER.debug("Add [%s][%s] to available translations", section, option)
+                PARSER.set(section, option, value)
+
+    if changed:
+        with io.open(PARSER.filename, 'w', encoding="utf-8") as fp:
+            PARSER.write(fp)
+
+
+def edit():
+    """Open a text editor to edit the translations.
+    """
+    if not getattr(PARSER, 'filename', None):
+        raise EnvironmentError("Translation system is not initialized")
+
+    open_text_editor(PARSER.filename)
+
+
+def get_supported_languages():
+    """Return the list of supported language.
+    """
+    if getattr(PARSER, 'filename', None):
+        return list(sorted(lang for lang in PARSER.sections()))
+    return list(sorted(DEFAULT.keys()))
+
+
+def get_translated_text(key):
+    """Return the text corresponding to the key in the language defined in the config.
+
+    :param key: key in the translation file
+    :type key: str
+    """
+    if not getattr(PARSER, 'filename', None):
+        raise EnvironmentError("Translation system is not initialized")
+
+    if PARSER.has_section(CURRENT) and PARSER.has_option(CURRENT, key):
+        return PARSER.get(CURRENT, key).strip('"')
+    elif PARSER.has_option('en', key):
+        LOGGER.warning("Unsupported language '%s', fallback to English", CURRENT)
+        return PARSER.get('en', key).strip('"')
+
+    LOGGER.debug("No translation defined for '%s/%s' key", CURRENT, key)
+    return None
