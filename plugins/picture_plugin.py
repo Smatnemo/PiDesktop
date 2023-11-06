6590e67a3fa2492232295c408e5c607f771bb772
Tertese Moses <smatnemo25@gmail.com>
2023-11-06 19:11:23 +0000

    Created PicturePlugin class

---
+++ plugins/picture_plugin.py
@@ -0,0 +1,12 @@
+
+import os
+import os.path as osp
+import itertools
+from datetime import datetime
+import pibooth
+from LDS.utils import LOGGER, PoolingTimer
+# from LDS.pictures import get_picture_factory
+# from LDS.pictures.pool import PicturesFactoryPool
+
+class PicturePlugin(object):
+    pass
