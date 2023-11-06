d6f3c57f9cf7face5a5fbd5dcf9e0875f26d6ec4
Tertese Moses <smatnemo25@gmail.com>
2023-11-06 09:51:07 +0000

    imported LDS and created the required states

--- legal.py
+++ legal.py
@@ -1,10 +1,29 @@
-from lds.database import DataBase 
-from lds.utils import LOGGER
-from lds.view.window import PiWindow
+from LDS.database import DataBase 
+from LDS.utils import LOGGER
+from LDS.view.window import PiWindow
+from LDS.states import StatesMachine
+
+
 
 class PiApplication:
+
     def __init__(self):
-        pass 
+       # Define states of the application
+        self._machine = StatesMachine(self._pm, self._config, self, self._window)
+        self._machine.add_state('videoplayback') # Add this state
+        self._machine.add_state('wait')
+        self._machine.add_state('beginsession') # Start - To do
+        self._machine.add_state('login')
+        self._machine.add_state('choose')
+        self._machine.add_state('chosen')
+        self._machine.add_state('preview')
+        self._machine.add_state('capture')
+        self._machine.add_state('showcapture') # work on showing the captured image
+        self._machine.add_state('processing')
+        self._machine.add_state('print')
+        self._machine.add_state('finish')
+        self._machine.add_state('reprint') # Add a reprint option and state.
+        self._machine.add_state('endsession') # End the session
 
     def _initialize(self):
         pass 
