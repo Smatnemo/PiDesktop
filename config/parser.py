261c7000a3686156987952c727ff902c9a74aafa
Tertese Moses <smatnemo25@gmail.com>
2023-11-06 20:56:41 +0000

    Created values_list_repr

--- config/parser.py
+++ config/parser.py
@@ -13,6 +13,13 @@ from collections import OrderedDict as odict
 from LDS.utils import LOGGER, open_text_editor
 from LDS import language
 
+
+def values_list_repr(values):
+    """Concatenate a list of values to a readable string.
+    """
+    return "'{}' or '{}'".format("', '".join([str(i) for i in values[:-1]]), values[-1])
+
+
 class PiConfigParser(object):
     
     def __init__(self,):
