f608f1e040ae0921a250cded08490020cc328674
Tertese Moses <smatnemo25@gmail.com>
2023-11-07 09:41:21 +0000

    Defined initialization and state_wait_enter function for LightsPlugin

--- plugins/lights_plugin.py
+++ plugins/lights_plugin.py
@@ -2,4 +2,20 @@ import time
 import LDS
 
 class LightsPlugin(object):
-    pass
+
+
+    name = "LDS-core:lights"
+
+    def __init__(self,plugin_manager):
+        self._pm = plugin_manager
+        self.blink_time = 0.3
+    
+    @LDS.hookimpl
+    def state_wait_enter(self, app):
+        if app.previous_picture_file and app.printer.is_ready()\
+                and app.count.remaining_duplicates > 0:
+            app.leds.blink(on_time=self.blink_time, off_time=self.blink_time)
+        else:
+            app.leds.capture.blink(on_time=self.blink_time, off_time=self.blink_time)
+            app.leds.printer.off()
+    
