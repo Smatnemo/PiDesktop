b73df823aba25cdfaf823ddeae7c641ddbbfe0be
Tertese Moses <smatnemo25@gmail.com>
2023-11-07 13:47:04 +0000

    Created state_processing do function

--- plugins/picture_plugin.py
+++ plugins/picture_plugin.py
@@ -93,3 +93,41 @@ class PicturePlugin(object):
         self.second_previous_picture = app.previous_picture
         self._reset_vars(app)
 
+    @LDS.hookimpl
+    def state_processing_do(self, cfg, app):
+        idx = app.capture_choices.index(app.capture_nbr)
+        self.texts_vars['date'] = datetime.strptime(app.capture_date, "%Y-%m-%d-%H-%M-%S")
+        self.texts_vars['count'] = app.count
+
+        LOGGER.info("Saving raw captures")
+        captures = app.camera.get_captures()
+
+        for savedir in cfg.gettuple('GENERAL', 'directory', 'path'):
+            rawdir = osp.join(savedir, "raw", app.capture_date)
+            os.makedirs(rawdir)
+
+            for capture in captures:
+                count = captures.index(capture)
+                capture.save(osp.join(rawdir, "pibooth{:03}.jpg".format(count)))
+
+        LOGGER.info("Creating the final picture")
+        default_factory = get_picture_factory(captures, cfg.get('PICTURE', 'orientation'))
+        factory = self._pm.hook.pibooth_setup_picture_factory(cfg=cfg,
+                                                              opt_index=idx,
+                                                              factory=default_factory)
+        app.previous_picture = factory.build()
+
+        for savedir in cfg.gettuple('GENERAL', 'directory', 'path'):
+            app.previous_picture_file = osp.join(savedir, app.picture_filename)
+            factory.save(app.previous_picture_file)
+
+        if cfg.getboolean('WINDOW', 'animate') and app.capture_nbr > 1:
+            LOGGER.info("Asyncronously generate pictures for animation")
+            for capture in captures:
+                default_factory = get_picture_factory((capture,), cfg.get(
+                    'PICTURE', 'orientation'), force_pil=True, dpi=200)
+                factory = self._pm.hook.pibooth_setup_picture_factory(cfg=cfg,
+                                                                      opt_index=idx,
+                                                                      factory=default_factory)
+                factory.set_margin(factory._margin // 3)  # 1/3 since DPI is divided by 3
+                self.factory_pool.add(factory)
