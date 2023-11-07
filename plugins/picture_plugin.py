83291936bdb8da2899e1808a47ed53f22d301cbb
Tertese Moses <smatnemo25@gmail.com>
2023-11-07 13:41:52 +0000

    Created pibooth_setup_picture_factory function

--- plugins/picture_plugin.py
+++ plugins/picture_plugin.py
@@ -28,3 +28,35 @@ class PicturePlugin(object):
         app.previous_picture = None
         app.previous_animated = None
         app.previous_picture_file = None
+
+    @pibooth.hookimpl(hookwrapper=True)
+    def pibooth_setup_picture_factory(self, cfg, opt_index, factory):
+
+        outcome = yield  # all corresponding hookimpls are invoked here
+        factory = outcome.get_result() or factory
+
+        factory.set_margin(cfg.getint('PICTURE', 'margin_thick'))
+
+        backgrounds = cfg.gettuple('PICTURE', 'backgrounds', ('color', 'path'), 2)
+        factory.set_background(backgrounds[opt_index])
+
+        overlays = cfg.gettuple('PICTURE', 'overlays', 'path', 2)
+        if overlays[opt_index]:
+            factory.set_overlay(overlays[opt_index])
+
+        texts = [cfg.get('PICTURE', 'footer_text1').strip('"').format(**self.texts_vars),
+                 cfg.get('PICTURE', 'footer_text2').strip('"').format(**self.texts_vars)]
+        colors = cfg.gettuple('PICTURE', 'text_colors', 'color', len(texts))
+        text_fonts = cfg.gettuple('PICTURE', 'text_fonts', str, len(texts))
+        alignments = cfg.gettuple('PICTURE', 'text_alignments', str, len(texts))
+        if any(elem != '' for elem in texts):
+            for params in zip(texts, text_fonts, colors, alignments):
+                factory.add_text(*params)
+
+        if cfg.getboolean('PICTURE', 'captures_cropping'):
+            factory.set_cropping()
+
+        if cfg.getboolean('GENERAL', 'debug'):
+            factory.set_outlines()
+
+        outcome.force_result(factory)
