e43355459c4003e69a949cc736ae58faa5677b2a
Tertese Moses <smatnemo25@gmail.com>
2023-11-07 11:25:55 +0000

    Created constructor for ViewPlugin class

--- plugins/view_plugin.py
+++ plugins/view_plugin.py
@@ -2,4 +2,26 @@ import LDS
 from LDS.utils import LOGGER, get_crash_message, PoolingTimer
 
 class ViewPlugin(object):
-    pass
+
+    """Plugin to manage the LDS window dans transitions.
+    """
+
+    name = 'LDS-core:view'
+    
+
+    def __init__(self, plugin_manager):
+        self._pm = plugin_manager
+        self.count = 0
+        self.forgotten = False
+        # Seconds to display the failed message
+        self.failed_view_timer = PoolingTimer(2)
+        # Seconds between each animated frame
+        self.animated_frame_timer = PoolingTimer(0)
+        # Seconds before going back to the start
+        self.choose_timer = PoolingTimer(30)
+        # Seconds to display the selected layout
+        self.layout_timer = PoolingTimer(4)
+        # Seconds to display the selected layout
+        self.print_view_timer = PoolingTimer(0)
+        # Seconds to display the selected layout
+        self.finish_timer = PoolingTimer(1)
