037038c2a7c80490d1715307b98cf292057172c6
Tertese Moses <smatnemo25@gmail.com>
2023-11-18 17:21:11 +0000

    Added variables to hold chosen row for inmates

--- view/documentsview.py
+++ view/documentsview.py
@@ -1,92 +1,167 @@
+from tkinter.colorchooser import Chooser
 import pygame
 
 # Custom events for documents 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+CHOSEEVENT = pygame.USEREVENT + 16
 
 class InmateRow(object):
-    def __init__(self, inmate_number, documents):
-        self.inmate_number = inmate_number 
+    def __init__(self, inmate_number, documents, row_number):
+
         self.documents = documents
+        self.row_height = 60
+        self.inmate_rect = None
+        self.row_color = None 
+        self.row_text_color = BLACK
+        self.row_text_size = 32
 
-    def draw(self):
-        pass
+        self.row_num = row_number 
+        self.row_num_coord = None
+
+        self.num = len(documents)
+        self.num_coord = None
+
+        self.inmate_number = inmate_number 
+        self.inmate_num_coord = None
+
+        self.chosen = False
+
+
+        self.font = pygame.font.Font('freesansbold.ttf', self.row_text_size)
+
+    def draw(self, foreground_rect, screen, event):
+        self.inmate_rect = pygame.Rect(foreground_rect.x, foreground_rect.y+60*(self.row_num), foreground_rect.width, self.row_height)
+        # pygame.draw.rect(screen, 'dark gray', self.inmate_rect)
+        
+        if self.hovered(event):
+            pygame.draw.rect(screen, 'dark gray', self.inmate_rect)
+            clicked = self.clicked(event)
+            if clicked == 'BUTTONDOWN':
+                pygame.draw.rect(screen, 'black', self.inmate_rect)
+            elif clicked == 'BUTTONUP':
+                pygame.draw.rect(screen, 'dark gray', self.inmate_rect)
+        else:
+            pygame.draw.rect(screen, 'light gray', self.inmate_rect)
+        
+        screen.blit(self.text_surface(self.row_num+1), (foreground_rect.x+14,foreground_rect.y+60*(self.row_num)+14))
+        screen.blit(self.text_surface(self.inmate_number), (foreground_rect.x+54, foreground_rect.y+60*(self.row_num)+14))
+        screen.blit(self.text_surface(self.num), (foreground_rect.width//2, foreground_rect.y+60*(self.row_num)+14))
+
+    def text_surface(self, text):
+        text = str(text)
+        text_surface = self.font.render(text, True, self.row_text_color)
+        width, height = self.font.size(text)
+        return text_surface 
     
-    def update(self):
-        pass 
-
-    def handle_events(self, events):
-        pass
 
     def hovered(self, event):
         if not event:
             return None
-        if self.button_rect.collidepoint(event.pos) and self.button_enabled:
+        if self.inmate_rect.collidepoint(event.pos):
             return True 
         else:
             return None        
 
     def clicked(self, event):
-        self.button_text = self.font.render(self.label, True, self.button_color)
-        width, height = self.font.size(self.label)
-        self.button_rect.width, self.button_rect.height = width+6, height+6
-        
-        if self.button_enabled:
-            if self.hovered(event):
-                pygame.draw.rect(self.screen, 'dark gray', self.button_rect)
-                clicked = self.clicked(event)
-                if clicked == 'BUTTONDOWN':
-                    pygame.draw.rect(self.screen, 'black', self.button_rect)
-                elif clicked == 'BUTTONUP':
-                    pygame.draw.rect(self.screen, 'dark gray', self.button_rect)
-            else:
-                pygame.draw.rect(self.screen, 'light gray', self.button_rect)
+        if not event:
+            return None
+        if event.type==pygame.MOUSEBUTTONDOWN and self.inmate_rect.collidepoint(event.pos):  
+            return 'BUTTONDOWN'
+        if event.type==pygame.MOUSEBUTTONUP and self.inmate_rect.collidepoint(event.pos):
+            pygame.event.post(pygame.event.Event(CHOSEEVENT))
+            self.chosen = True
+            return 'BUTTONUP'
         else:
-            pygame.draw.rect(self.screen, 'blue', self.button_rect)
-        self.screen.blit(self.button_text, self.coord)
+            return None
         
 
 
 class DocumentRow(object):
-    def __init__(self, document:tuple):
-        pass
+    def __init__(self, document:tuple, row_number:int):
+        self.document_rect = None
+        self.row_height = 60
+        self.row_num = row_number
+        self.document_name = "Document"+str(self.row_num+1)
+        self.status = document[-1]
+        self.row_text_size = 32
+        self.row_text_color = BLACK
+        self.font = pygame.font.Font('freesansbold.ttf', self.row_text_size)
 
-    def draw(screen):
-        pass
+    def draw(self, foreground_rect, screen, event):
+        self.document_rect = pygame.Rect(foreground_rect.x, foreground_rect.y+60*(self.row_num), foreground_rect.width, self.row_height)
 
-    def handle_events(self, events):
-        pass 
+        if self.hovered(event):
+            pygame.draw.rect(screen, 'dark gray', self.document_rect)
+            clicked = self.clicked(event)
+            if clicked == 'BUTTONDOWN':
+                pygame.draw.rect(screen, 'black', self.document_rect)
+            elif clicked == 'BUTTONUP':
+                pygame.draw.rect(screen, 'dark gray', self.document_rect)
+        else:
+            pygame.draw.rect(screen, 'light gray', self.document_rect)
+
+        screen.blit(self.text_surface(self.row_num+1), (foreground_rect.x+14,foreground_rect.y+60*(self.row_num)+14))
+        screen.blit(self.text_surface(self.document_name), (foreground_rect.x+54, foreground_rect.y+60*(self.row_num)+14))
+        screen.blit(self.text_surface(self.status), (foreground_rect.width//2, foreground_rect.y+60*(self.row_num)+14))
+
+
+    def text_surface(self, text):
+        text = str(text)
+        text_surface = self.font.render(text, True, self.row_text_color)
+        width, height = self.font.size(text)
+        return text_surface     
 
     def hovered(self, event):
-        pass 
+        if not event:
+            return None
+        if self.document_rect.collidepoint(event.pos):
+            return True 
+        else:
+            return None 
 
     def clicked(self, event):
-        pass
+        if not event:
+            return None
+        if event.type==pygame.MOUSEBUTTONDOWN and self.document_rect.collidepoint(event.pos):  
+            return 'BUTTONDOWN'
+        if event.type==pygame.MOUSEBUTTONUP and self.document_rect.collidepoint(event.pos):
+            pygame.event.post(pygame.event.Event(CHOSEEVENT))
+            return 'BUTTONUP'
+        else:
+            return None
 
 
 
 class DocumentsView(object):
-    def __init__(self, documents):
-        self.document_rows = [DocumentRow(document) for document in documents]
+    def __init__(self, inmate_documents, selected):
+        self.inmate_documents = inmate_documents
+        documents = inmate_documents[selected]
+        self.document_rows = [DocumentRow(document, doc_num) for doc_num, document in enumerate(documents)]
     
-    def draw(self, screen, foreground_rect):
+    def draw(self, foreground_rect, screen):
         for document_row in self.document_rows:
-            document_row.draw(screen)
+            document_row.draw(foreground_rect, screen)
 
 
 class InmateDocumentsView(object):
     def __init__(self, inmate_documents):
         self.inmate_numbers = list(inmate_documents.keys())
-        self.inmate_rows = [InmateRow(inmate_number, inmate_documents[inmate_number]) for inmate_number in self.inmate_numbers]
-
+        self.inmate_rows = [InmateRow(inmate_number, inmate_documents[inmate_number], row_number) for row_number, inmate_number in enumerate(self.inmate_numbers)]
+        self.update_needed = None
+        self.choseninmaterow = None
 
     def draw(self, foreground_rect, screen):
         for inmate_row in self.inmate_rows:
-            inmate_row.draw(screen, foreground_rect)
+            inmate_row.draw(foreground_rect, screen, self.update_needed)
 
     def update(self):
-        pass
+        # When button is clicked, return the inmate number
+        for row in self.inmate_rows:
+            if row.chosen == True:
+                self.choseninmaterow = row
+                row.chosen = False
 
 
 
