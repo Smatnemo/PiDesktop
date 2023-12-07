fb62194e08a808e3de98da90b8e6c54d80e5f19e
Tertese Moses <smatnemo25@gmail.com>
2023-12-07 14:33:27 +0000

    Added the changes from the main app

--- plugins/view_plugin.py
+++ plugins/view_plugin.py
@@ -2,6 +2,7 @@ import LDS
 import os
 import sys
 import pygame
+import time
 from LDS.utils import LOGGER, get_crash_message, PoolingTimer
 from LDS.accounts import LogIn, LogOut
 from LDS.documents.document import decrypt_content, document_authentication
@@ -35,6 +36,8 @@ class ViewPlugin(object):
         self.finish_timer = PoolingTimer(1)
         # Lock screen timer for failed login or failed decryption after 3 attempts
         self.lock_screen_timer = PoolingTimer(30)
+        # Create a delay before allowing the next state
+        self.delay_state_timer = PoolingTimer(2)
 
         self.login_view = None 
         self.decrypt_view = None
@@ -64,13 +67,13 @@ class ViewPlugin(object):
     def state_wait_validate(self, cfg, app, events):
         event = app.find_screen_event(events)
         if event:
-            LOGGER.info("This is the event from wait state:{}".format(event))
             return 'login'
       
 
     @LDS.hookimpl
     def state_wait_exit(self, win):
         self.count = 0
+        pygame.event.clear()
         win.show_image(None)  # Clear currently displayed image
 
     @LDS.hookimpl
@@ -85,8 +88,10 @@ class ViewPlugin(object):
     def state_login_do(self, app, win, events):
         
         if events:
-            # LOGGER.info("Events:{}".format(events))
             self.choose_timer.start()
+
+        app.find_touch_effects_event(events)
+
         if app.find_login_event(events):
             if self.login_view.passcode_box.input_text != '':
                 app.password = self.login_view.passcode_box.input_text 
@@ -94,6 +99,7 @@ class ViewPlugin(object):
                 app.password = self.login_view.get_input_text() 
             self.login_view.passcode_box.text=''
             self.login_view.passcode_box.txt_surface = self.login_view.passcode_box.font.render(self.login_view.passcode_box.text, True, self.login_view.passcode_box.color)
+        
         self.login_view.update_needed = app.update_needed
         self.login_view.passcode_box.handle_event(events)
         self.login_view.draw(win.surface)
@@ -137,6 +143,8 @@ class ViewPlugin(object):
         if events:
             self.choose_timer.start()
 
+        app.find_touch_effects_event(events)
+
         win._current_documents_foreground.inmate_documents_view.update_needed = app.update_needed
         win.show_choices(app.documents)
 
@@ -149,10 +157,13 @@ class ViewPlugin(object):
         event = app.find_choose_event(events)
         if event:
             app.inmate_number = win._current_documents_foreground.inmate_documents_view.choseninmaterow.inmate_number
+
+        next_previous_foreground_event = app.find_next_back_event(events)
    
     @LDS.hookimpl
     def state_choose_validate(self, cfg, app, events):
-        if app.find_next_back_event(events):
+        event = app.find_next_back_event(events)
+        if event.back:
             return 'login'
         elif app.find_lockscreen_event(events):
             app.previous_state = 'choose'
@@ -299,7 +310,6 @@ class ViewPlugin(object):
     def state_lock_enter(self,app,win):
         LOGGER.info("This is the lock state")
         self.lock_screen_timer.start()
-        
         win.show_locked('locked', app.attempt_count)
 
     @LDS.hookimpl
@@ -312,7 +322,6 @@ class ViewPlugin(object):
             return 'login' 
 
     # ----------------------------- PassFail State           --------------------------------
-
     @LDS.hookimpl
     def state_passfail_enter(self,app,win):
         LOGGER.info("This is the passfail state")
