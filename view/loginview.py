ddb0f2d119a43826d1c30d1bf3f4de30226ceba0
Jambo <59573022+Jim-2e-volve@users.noreply.github.com>
2023-12-12 16:12:37 +0000

    Fixing* Resolution for Pi 7" Screen Official

--- view/loginview.py
+++ view/loginview.py
@@ -8,11 +8,12 @@ from LDS import fonts, pictures
 from LDS.media import get_filename
 
 pygame.init()
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 1024
+HEIGHT = 600
 DEFAULT_SIZE = WIDTH, HEIGHT
-vid_info = pygame.display.Info()
-vid_size = vid_info.current_w, vid_info.current_h
+vid_size = (WIDTH, HEIGHT)
+#vid_info = pygame.display.Info()
+#vid_size = vid_info.current_w, vid_info.current_h
 screen = pygame.display.set_mode(vid_size)
 COLOR_INACTIVE = pygame.Color('lightskyblue3')
 COLOR_ACTIVE = pygame.Color('dodgerblue2')
